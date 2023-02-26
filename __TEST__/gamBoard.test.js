ae904dade945eadcd0796509407bbc4a863397a3
Gibsongf <maccgiiibson@gmail.com>
2023-02-25 22:32:04 -0300

    change place ship test

--- __TEST__/gamBoard.test.js
+++ __TEST__/gamBoard.test.js
@@ -6,18 +6,32 @@ const board = new GameBoard();
 
 describe("Place ship from GameBoard", () => {
 	let coordinates = "4, 5";
-	const receive ={
-		right: [[4, 5],[4, 6],[4, 7]],
-		left: [[4, 5],[4, 4],[4, 3]],
-		up: [[4, 5],[3, 5],[2, 5]],
-		down:[[4, 5],[5, 5],[6, 5]]
-	}
-	test("put ship somewhere", () => {
-		
-		expect(board.placeShip(aShip, coordinates).left).toStrictEqual(receive.left);
-		expect(board.placeShip(aShip, coordinates).right).toStrictEqual(receive.right);
-		expect(board.placeShip(aShip, coordinates).up).toStrictEqual(receive.up);
-		expect(board.placeShip(aShip, coordinates).down).toStrictEqual(receive.down);
+	const receive = {
+		right : [[4, 5],[4, 6],[4, 7]],
+		left : [[4, 5],[4, 4],[4, 3]],
+		up : [[4, 5],[3, 5],[2, 5]],
+		down : [[4, 5],[5, 5],[6, 5]]
+	};
+	test('new get coord', () => {
+		expect(board.getCoord(coordinates,aShip).right).toStrictEqual(receive.right);
+		expect(board.getCoord(coordinates,aShip).left).toStrictEqual(receive.left);
+		expect(board.getCoord(coordinates,aShip).up	).toStrictEqual(receive.up);
+		expect(board.getCoord(coordinates,aShip).down).toStrictEqual(receive.down);
+	});
+	test.only("put ship somewhere and register tha place at board obj", () => {
+		const checkBoard = (arr) => {
+			let allSaves = true
+			arr.forEach((num) => {
+				if(board.board[num[0]][num[1]] === " "){
+					allSaves = false
+				}
+			});
+			return allSaves
+		}
+		;
+		expect(board.placeShip(coordinates,aShip,true,'left')).toStrictEqual(receive.left);
+		expect(checkBoard(receive.left)).toBe(true);
+		expect(checkBoard(receive.right)).toBe(false);
 	});
 	test("Receive undefined in invalid direction", () => {
 		let coord = '0,0';
@@ -25,13 +39,13 @@ describe("Place ship from GameBoard", () => {
 		expect(board.placeShip(aShip, coord).left).toStrictEqual(undefined);
 	});
 	test("Position already used",() => {
-		board.placeShip(aShip, coordinates, true).left
-		expect(board.placeShip(aShip, coordinates)).toStrictEqual('POSITION ALREADY USED');
-		expect(board.placeShip(aShip, coordinates).left).toStrictEqual(undefined)
+		board.placeShip(aShip, coordinates, true,'left')
+		expect(board.placeShip(aShip, coordinates,true,'left')).toStrictEqual('POSITION ALREADY USED');
+		
 	})
 });
 
-describe("receiveAttack functionality", () => {
+/* describe("receiveAttack functionality", () => {
 	const atkCoord = '2,1';
 	board.placeShip(aShip, '2,1', true).left
 	test("ship get hit by receiveAttack", () => {
@@ -45,4 +59,4 @@ describe("receiveAttack functionality", () => {
 	test("receiveAttack missed", () => {
 		expect(board.receiveAttack('7,1')).toStrictEqual("Missed");
 	});
-});
+}); */
--- __TEST__/ships.test.js
+++ __TEST__/ships.test.js
@@ -1,8 +1,7 @@
 import { completeShip} from "../src/ships.js";
 
 const aShip = completeShip('Submarine');
-test.todo('')
-/* test('Increase of hit number',() => {
+test('Increase of hit number',() => {
     expect(aShip.hit()).toBe(1)
     expect(aShip.hit()).toBe(2)
     expect(aShip.hit()).toBe(3)
@@ -11,4 +10,4 @@ test.todo('')
 test('Check if the ship was sunk',() => {
     expect(aShip.isSunk()).toBe(true)
     expect(aShip.sunk).toBe(true)
-}) */
+})
